a1afec741862a37323118a05714cea47807562ed
sceint <arpanladani@gmail.com>
2017-04-14 22:18:53 +0530

    Online + Offline (Changed the webhost) resolved the dulicate Id issue

--- app/src/main/java/com/example/admin/feedback/OnlineDBHelper.java
+++ app/src/main/java/com/example/admin/feedback/OnlineDBHelper.java
@@ -16,57 +16,20 @@ import java.net.URL;
 public class OnlineDBHelper {
 
     void uploadParentData(JSONArray jsonArray) {
-
-        class WriteData extends AsyncTask<JSONArray, Void, Void> {
-            @Override
-            protected Void doInBackground(JSONArray... resultArray) {
-                try {
-                    String login_url = "http://sceint.5gbfree.com/JSON2DBParent.php";
-
-                    URL url = new URL(login_url);
-                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                    httpURLConnection.setRequestMethod("POST");
-                    httpURLConnection.setDoOutput(true);
-                    httpURLConnection.setDoInput(true);
-
-                    OutputStream outputStream = httpURLConnection.getOutputStream();
-                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                    bufferedWriter.write(resultArray[0].toString());
-                    bufferedWriter.flush();
-                    bufferedWriter.close();
-                    outputStream.close();
-
-                    InputStream inputStream = httpURLConnection.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
-                    String s;
-                    StringBuilder sb = new StringBuilder("");
-                    while ((s = bufferedReader.readLine()) != null)
-                        sb.append(s);
-                    bufferedReader.close();
-                    inputStream.close();
-                    httpURLConnection.disconnect();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void s) {
-            }
-        }
-        WriteData writeData = new WriteData();
-        writeData.execute(jsonArray);
+        uploadData(jsonArray, "JSON2DBParent.php");
     }
 
     void uploadRatingData(JSONArray jsonArray) {
+        uploadData(jsonArray, "JSON2DBRating.php");
+    }
 
+    void uploadData(JSONArray jsonArray, final String url){
         class WriteData extends AsyncTask<JSONArray, Void, Void> {
+            String login_url = "https://sceint.000webhostapp.com/";
+
             @Override
             protected Void doInBackground(JSONArray... resultArray) {
                 try {
-                    String login_url = "http://sceint.5gbfree.com/JSON2DBRating.php";
-
                     URL url = new URL(login_url);
                     HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                     httpURLConnection.setRequestMethod("POST");
@@ -96,7 +59,8 @@ public class OnlineDBHelper {
             }
 
             @Override
-            protected void onPostExecute(Void s) {
+            protected void onPreExecute() {
+                login_url += url;
             }
         }
         WriteData writeData = new WriteData();
